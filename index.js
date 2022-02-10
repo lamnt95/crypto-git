0dbec9badff4bd8d15717a9717257cd463b35030
lamnt95 <tunglam20132208@gmail.com>
2022-02-10 14:35:55 +0700

    1 file modified

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 
 app.use(express.static('static'));
 app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// parse application/json
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.sendFile(path.resolve('pages/index.html'));
 });
